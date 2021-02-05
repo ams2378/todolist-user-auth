258e6711e5062e071dec372442533c6d7f7e0f41
Adil Sadik <adil@adils-air.home>
2021-02-04 21:30:02 -0500

    password encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ApplicationRecord
 
     has_many :sessions
     has_many :tasks
+
+    after_validation :hash_password
+
+    
+    private
+    def hash_password
+      self.password = BCrypt::Password.create(self.password)
+    end
+
+
 end
